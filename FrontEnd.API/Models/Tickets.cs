b43ab33bcb4bbcec9e65daf2dccdbb2424f23104
LorianAg <loriana.aguilar02@gmail.com>
2021-04-10 11:42:49 -0600

    Creaciion clsse Tickets en models FrontEnd.API

---
+++ FrontEnd.API/Models/Tickets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.API.Models
+{
+    public class Tickets
+    {
+        public Tickets()
+        {
+            //BoleteriaReservados = new HashSet<BoleteriaReservados>();
+        }
+        public int CodTicket { get; set; }
+        public string Nreserva { get; set; }
+        public string Usuario { get; set; }
+        public int CodEmpresa { get; set; }
+        public DateTime Fecha { get; set; }
+        public int? Estado { get; set; }
+    }
+}
