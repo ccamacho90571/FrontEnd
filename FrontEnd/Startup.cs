f1ac5fcec91efeba9b1c86024261c7e419346ebc
LorianAg <loriana.aguilar02@gmail.com>
2021-04-10 11:39:37 -0600

    Modificacion Startup FrontEnd (wizard)

--- FrontEnd/Startup.cs
+++ FrontEnd/Startup.cs
@@ -1,13 +1,16 @@
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace FrontEnd
 {
@@ -23,7 +26,11 @@ namespace FrontEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CRPassContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("ConnectionAPI")));
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
