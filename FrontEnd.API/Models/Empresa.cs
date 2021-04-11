facc7599bf383aba93b0eed21d023d29da39105c
LorianAg <loriana.aguilar02@gmail.com>
2021-04-11 11:51:34 -0600

    Creacion de la clase Empresa en Models

---
+++ FrontEnd.API/Models/Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.API.Models
+{
+    public class Empresa
+    {
+        public int CodEmpresa { get; set; }
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public int ReservasUsuario { get; set; }
+
+        public virtual ICollection<Boleteria> Boleteria { get; set; }
+        public virtual ICollection<ControlAforo> ControlAforo { get; set; }
+        public virtual ICollection<Publicidad> Publicidad { get; set; }
+        public virtual ICollection<Tickets> Tickets { get; set; }
+        public virtual ICollection<Usuarios> Usuarios { get; set; }
+    }
+}
