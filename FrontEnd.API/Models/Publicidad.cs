cf1aa29b948ae437624adfcd52d8a2606f3b4492
LorianAg <loriana.aguilar02@gmail.com>
2021-04-11 11:45:51 -0600

    Creacion clase Publicidad en models

---
+++ FrontEnd.API/Models/Publicidad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.API.Models
+{
+    public class Publicidad
+    {
+        public int CodPublicidad { get; set; }
+        public int CodEmpresa { get; set; }
+        public string RutaArchivo { get; set; }
+
+        public virtual Empresa CodEmpresaNavigation { get; set; }
+    }
+}
