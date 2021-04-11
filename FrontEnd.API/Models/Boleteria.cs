4b44f1618f6bdd76d732e9d07c4e3d91eb7cae25
LorianAg <loriana.aguilar02@gmail.com>
2021-04-11 11:55:23 -0600

    Creacion clase Boleteria en Models

---
+++ FrontEnd.API/Models/Boleteria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.API.Models
+{
+    public class Boleteria
+    {
+        public int CodBoleteria { get; set; }
+        public int CodEmpresa { get; set; }
+        public string Descripcion { get; set; }
+        public int Costo { get; set; }
+
+        public virtual Empresa CodEmpresaNavigation { get; set; }
+        public virtual ICollection<BoleteriaReservados> BoleteriaReservados { get; set; }
+    }
+}
